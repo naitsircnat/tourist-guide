0e7c9bd49b8177e819bdba8814f2912544530741
naitsircnat <cristiangerardchen@gmail.com>
2024-11-16 12:08:06 +0800

    Added planning pointers in the form of comments

--- script.js
+++ script.js
@@ -71,8 +71,16 @@ window.addEventListener("DOMContentLoaded", async function () {
 });
 
 /*
-FEATURES FLOW:
-- Can select these to view on map
+PROJECT PHASES
+- RESEARCH IDEAS TO DECIDE UI LAYOUT
+- IMPLEMENT CORE FEATURES
+- CREATE UI
+- POLISH
+
+
+FEATURES/LAYOUT?
+- map
+- selectors of what things to display
 --attractions 
 --hotels 
 --nature 
@@ -81,7 +89,26 @@ FEATURES FLOW:
 --transport 
 --supermarket 
 --hawker centres
---food (too many to display) 
+--food (too many to display)
+--airports 
 
 - Can do custom search for places (Foursquare) - e.g. user's hotel or place of interest
+
+Menu items
+- history?
+- economy?
+- geography?
+https://99designs.com.sg/profiles/276825/designs/459509
+can refer to london map site for other features
+
+
+RESOURCES
+- requirements: https://docs.google.com/document/d/1iVANh3aaqpX0VSFscP7j-86M1BOkAMGtOuJRmnYynD0/edit?tab=t.0#heading=h.5gcv0ns1dl74
+- foursquare search functionality: https://scrawny-dingo-56a.notion.site/Foursquare-Leaflet-5c1bb194ca094dce895e7c02d8ae83d5
+- leaflet example tutorials: 
+https://leafletjs.com/examples.html
+https://docs.maptiler.com/leaflet/examples/
+
+
+
 */
