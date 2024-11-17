715c1071d3ee2a07c38bec94e2a1ae83d02e7220
naitsircnat <cristiangerardchen@gmail.com>
2024-11-17 18:28:58 +0800

    Added data.js file and search function within

---
+++ data.js
@@ -0,0 +1,20 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = "fsq3nVm3/H75FWiXw0E7cysCDXelkbkAfUSN7R9m3jpO8Cc=";
+
+async function search(lat, lng, searchTerms) {
+  const response = await axios.get(`${BASE_API_URL}/places/search`, {
+    params: {
+      query: encodeURI(searchTerms), //encodeURI is used to convert special characters to their encoded eqv so that query will be wellformed
+      ll: lat + "," + lng,
+      sort: "DISTANCE",
+      radius: 5000,
+      limit: 50,
+    },
+    headers: {
+      Accept: "application/json",
+      // Provide the API key here
+      Authorization: API_KEY,
+    },
+  });
+  return response.data;
+}
